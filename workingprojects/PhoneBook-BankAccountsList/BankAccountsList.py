4ab55ef33a007a14d2ad643bd75cd2e1806ca299
mrt-cn <100875479+mrt-cn@users.noreply.github.com>
2022-03-11 17:49:19 +0300

    Delete workingprojects directory

--- workingprojects/PhoneBook-BankAccountsList/BankAccountsList.py
+++
@@ -1,4 +0,0 @@
-import MainProgram as dos
-dos.adres ="Account.csv"
-dos.alanlar = ["Banka Adı","Hesap Türü","Tutar","İşlem"]
-dos.Menu()
--- workingprojects/PhoneBook-BankAccountsList/PhoneBook.py
+++
@@ -1,4 +0,0 @@
-import MainProgram as dos
-dos.adres ="phonebook.csv"
-dos.alanlar = ["Adı Giriniz","Soyadı Gİriniz","Telefon Giriniz"]
-dos.Menu()
